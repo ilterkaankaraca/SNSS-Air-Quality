c54a0dcab0e6ffee6b07f1812c3f3ff7bc4da90b
Ilterkaan Karaca <ilterkaankaraca@gmail.com>
2021-06-25 21:30:02 +0200

    fix format

--- src/Air Quality/MWM/Model/AirMetrics.cs
+++ src/Air Quality/MWM/Model/AirMetrics.cs
@@ -8,15 +8,15 @@ namespace AirQuality.MWM.Model
 {
     class AirMetrics
     {
-    private double temperature, humidity, co2, airQuality, pressure, particle;
+        private double temperature, humidity, co2, airQuality, pressure, particle;
 
-    public double Temperature { get; set; }
-    public double Humidity { get; set; }
-    public double Co2 { get; set; }
-    public double AirQuality { get; set; }
-    public double Pressure { get; set; }
-    public double Particle { get; set; }
+        public double Temperature { get; set; }
+        public double Humidity { get; set; }
+        public double Co2 { get; set; }
+        public double AirQuality { get; set; }
+        public double Pressure { get; set; }
+        public double Particle { get; set; }
 
     }
-    
+
 }
