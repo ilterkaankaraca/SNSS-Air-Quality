a7f214b351766d995f3d6703db5a22ef077c0deb
Ilterkaan Karaca <ilterkaankaraca@gmail.com>
2021-07-01 16:53:07 +0200

    add outdoortvoc

--- src/arduino/server/OwmApi.h
+++ src/arduino/server/OwmApi.h
@@ -3,7 +3,7 @@
 #include <Wire.h>
 #include <ArduinoJson.h>
 
-extern float outdoorTemperature, outdoorPressure, outdoorHumidity, outdoorPm25, outdoorPm10, outdoorCo2 latitude, longitude;
+extern float outdoorTemperature, outdoorPressure, outdoorHumidity, outdoorPm25, outdoorPm10, outdoorCo2, outdoorTvoc, latitude, longitude;
 
 StaticJsonDocument<1024> weatherMetrics;
 StaticJsonDocument<1024> pollutionMetrics;
@@ -14,7 +14,7 @@ String getWeatherJson(String city)
   if ((WiFi.status() == WL_CONNECTED))
   { //Check the current connection status
     HTTPClient http;
-    http.begin("http://pro.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + API_KEY+"&units=metric");
+    //http.begin("http://pro.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + API_KEY+"&units=metric");
     httpCode = http.GET(); //Make the request
 
     if (httpCode > 0)
@@ -27,8 +27,8 @@ String getWeatherJson(String city)
     }
     http.end(); //Free the resources
   }
-  return payload;
-  //return "{\"coord\":{\"lon\":8.6728,\"lat\":49.9933},\"weather\":[{\"id\":801,\"main\":\"Clouds\",\"description\":\"few clouds\",\"icon\":\"02d\"}],\"base\":\"stations\",\"main\":{\"temp\":23.31,\"feels_like\":23.37,\"temp_min\":18.82,\"temp_max\":25.79,\"pressure\":1012,\"humidity\":64},\"visibility\":10000,\"wind\":{\"speed\":0.45,\"deg\":149,\"gust\":2.68},\"clouds\":{\"all\":20},\"dt\":1624908040,\"sys\":{\"type\":2,\"id\":2005640,\"country\":\"DE\",\"sunrise\":1624850314,\"sunset\":1624909102},\"timezone\":7200,\"id\":2881279,\"name\":\"Langen\",\"cod\":200}";
+  //return payload;
+  return "{\"coord\":{\"lon\":8.6728,\"lat\":49.9933},\"weather\":[{\"id\":801,\"main\":\"Clouds\",\"description\":\"few clouds\",\"icon\":\"02d\"}],\"base\":\"stations\",\"main\":{\"temp\":23.31,\"feels_like\":23.37,\"temp_min\":18.82,\"temp_max\":25.79,\"pressure\":1012,\"humidity\":64},\"visibility\":10000,\"wind\":{\"speed\":0.45,\"deg\":149,\"gust\":2.68},\"clouds\":{\"all\":20},\"dt\":1624908040,\"sys\":{\"type\":2,\"id\":2005640,\"country\":\"DE\",\"sunrise\":1624850314,\"sunset\":1624909102},\"timezone\":7200,\"id\":2881279,\"name\":\"Langen\",\"cod\":200}";
 }
 
 String getPollutionJson(float lat, float lon)
@@ -38,7 +38,7 @@ String getPollutionJson(float lat, float lon)
   if ((WiFi.status() == WL_CONNECTED))
   { //Check the current connection status
     HTTPClient http;
-    http.begin("http://pro.openweathermap.org/data/2.5/air_pollution?lat="+String(lat)+"&lon="+lon+"&appid="+API_KEY);
+   // http.begin("http://pro.openweathermap.org/data/2.5/air_pollution?lat="+String(lat)+"&lon="+lon+"&appid="+API_KEY);
     httpCode = http.GET(); //Make the request
     if (httpCode > 0)
     { //Check for the returning code
@@ -50,8 +50,8 @@ String getPollutionJson(float lat, float lon)
     }
     http.end(); //Free the resources
   }
-  return payload;
-  // return "{\"coord\":{\"lon\":49.99,\"lat\":8.67},\"list\":[{\"main\":{\"aqi\":5},\"components\":{\"co\":191.93,\"no\":0,\"no2\":0.06,\"o3\":23.96,\"so2\":0.08,\"pm2_5\":66.94,\"pm10\":359.45,\"nh3\":0.08},\"dt\":1624910400}]}";
+  //return payload;
+  return "{\"coord\":{\"lon\":49.99,\"lat\":8.67},\"list\":[{\"main\":{\"aqi\":5},\"components\":{\"co\":191.93,\"no\":0,\"no2\":0.06,\"o3\":23.96,\"so2\":0.08,\"pm2_5\":66.94,\"pm10\":359.45,\"nh3\":0.08},\"dt\":1624910400}]}";
 }
 
 void deserialize(String metricJson, char flag)
